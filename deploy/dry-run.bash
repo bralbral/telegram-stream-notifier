a0bdc3613d1f7da2f25eae5874020f7a629818e4
bralbral <bbralyc@gmail.com>
2023-10-15 21:12:12 +0300

    mvp6

--- deploy/dry-run.bash
+++ deploy/dry-run.bash
@@ -1,7 +1,12 @@
+if [[ ! $1 =~ ^[0-9]+$ ]]; then
+  echo "You need to create a superuser. Enter its telegram_id. This must be an integer number, for example: 1234567890"
+  exit 1
+fi
+export TELEGRAM_ID=$1
 echo "Run dry-run container"
 docker compose -f docker-compose-dry-run.yml up
 echo "Sleeping"
-sleep 15
+sleep 5
 docker cp dry-run:/app/youtube-notifier-bot.db .
 docker rm -f dry-run
 
