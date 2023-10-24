150f5b5f966303b1cea9eecee83da79f5f5265fe
bralbral <bbralyc@gmail.com>
2023-10-24 18:19:14 +0300

    Hotfix for docker image; speedup sqlite

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM python:3.11-slim-bullseye
+FROM python:3.11-slim-bookworm
 
 # Update the package list and install required packages
 RUN apt-get update && \
-    apt-get install -y git sqlite3  && \
+    apt-get install -y git && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
--- src/db/session.py
+++ src/db/session.py
@@ -1,3 +1,4 @@
+from sqlalchemy import event
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
@@ -7,6 +8,21 @@ from src.constants import SQLITE_DATABASE_FILE_PATH
 engine = create_async_engine(
     f"sqlite+aiosqlite:///{SQLITE_DATABASE_FILE_PATH}", future=True
 )
+
+
+# ================  Sqlite Speedup ==============================
+# Event listener function to set performance-related settings
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA synchronous = OFF")
+    cursor.execute("PRAGMA journal_mode = MEMORY")
+    cursor.execute("PRAGMA cache_size = 10000")
+    cursor.close()
+
+
+# Attach the event listener to the engine
+event.listen(engine.sync_engine, "connect", set_sqlite_pragma)
+# ================================================================
 
 session_maker: async_sessionmaker[AsyncSession] = async_sessionmaker(
     bind=engine, expire_on_commit=False, autoflush=True
