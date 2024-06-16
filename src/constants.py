2ff6d91c4c3921a745882b3293b461e4b9da45c9
bral bral <bbralyc@gmail.com>
2024-06-16 10:35:38 +0300

    Option for set SQLITE_DATABASE_FILE_PATH as ENV variable

--- src/constants.py
+++ src/constants.py
@@ -3,8 +3,10 @@ import os
 ROOT_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CONFIG_FILE_PATH: str = os.path.join(ROOT_DIR, "config.yaml")
 COOKIES_FILE_PATH: str = os.path.join(ROOT_DIR, "cookies.txt")
-SQLITE_DATABASE_FILE_PATH: str = os.path.join(ROOT_DIR, "youtube-notifier-bot.db")
-VERSION: str = "2024-06-15.14"
+SQLITE_DATABASE_FILE_PATH: str = os.environ.get(
+    "SQLITE_DATABASE_FILE_PATH", os.path.join(ROOT_DIR, "youtube-notifier-bot.db")
+)
+VERSION: str = "2024-06-16.10"
 
 __all__ = [
     "CONFIG_FILE_PATH",
