ca189199bbddc42fd67544eda88ec66fa0485a25
bral bral <bbralyc@gmail.com>
2024-06-16 10:37:16 +0300

    added option for setting CONFIG_FILE_PATH or COOKIES_FILE_PATH using ENV variables

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,12 @@
 import os
 
 ROOT_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CONFIG_FILE_PATH: str = os.path.join(ROOT_DIR, "config.yaml")
-COOKIES_FILE_PATH: str = os.path.join(ROOT_DIR, "cookies.txt")
+CONFIG_FILE_PATH: str = os.environ.get(
+    "CONFIG_FILE_PATH", os.path.join(ROOT_DIR, "config.yaml")
+)
+COOKIES_FILE_PATH: str = os.environ.get(
+    "COOKIES_FILE_PATH", os.path.join(ROOT_DIR, "cookies.txt")
+)
 SQLITE_DATABASE_FILE_PATH: str = os.environ.get(
     "SQLITE_DATABASE_FILE_PATH", os.path.join(ROOT_DIR, "youtube-notifier-bot.db")
 )
